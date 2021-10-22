334d730740c281cd5b20b321904efaacb018d146
emilioMarchi <em.98.nmynm@gmail.com>
2021-10-22 10:56:38 -0300

    nuevo commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Products</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Header;
