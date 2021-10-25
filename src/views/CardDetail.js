924c2b0057ac96191c1d7b8507e5efb71a244da6
emilioMarchi <em.98.nmynm@gmail.com>
2021-10-25 20:59:52 -0300

    nuevo commit

--- src/views/CardDetail.js
+++ src/views/CardDetail.js
@@ -3,17 +3,20 @@ import axios from 'axios';
 
 
 const CardDetail = ({ match }) => {
-    const [user, setUsers] = useState();
+    const [users, setUsers] = useState([]);
+    const [user, setUser] = useState([]);
 
 
     useEffect(() => {
             axios(`https://api.github.com/users`)
                 .then((res)=>{setUsers(res.data)})
-    }, [])
+    }, []);
 
     return (
         <div className='card-detail'>
-
+            {
+                users !=0 ? getUser() : console.log('out')
+            }
         </div>
     )
 }
